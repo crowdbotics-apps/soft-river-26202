8a9d48c9bb98ac988e9d20911579387af9e6b298
Crowdbotics <team@crowdbotics.com>
2021-05-05 22:39:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/soft_river_26202/MainActivity.java
@@ -0,0 +1,15 @@
+package com.soft_river_26202;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "soft_river_26202";
+  }
+}
